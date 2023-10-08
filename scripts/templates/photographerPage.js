7cdf312b1dfdd1d41a6b116af7f44884314ebd54
Charles <charles.jouenne@gmail.com>
2023-10-08 16:57:14 +0200

    Mise en place du photographeHeader

--- scripts/templates/photographerPage.js
+++ scripts/templates/photographerPage.js
@@ -10,27 +10,26 @@ function photographerTemplatePage(data) {
         img.setAttribute("src", picture)
         img.setAttribute("alt", `${name}`)
 
-        const photographersCardLink = document.createElement("a")
-        photographersCardLink.setAttribute("href", `photographer.html?id=${id}`)
-        photographersCardLink.className += "photographerLink"
+        const photographersCardLink = document.createElement("div")
+        photographersCardLink.className += "photographerDiv"
 
         const h2 = document.createElement( 'h2' )
         h2.textContent = name
 
         const location = document.createElement( 'p' )
         location.textContent = `${city}, ${country}`
-        location.className += "cityCountry"
+        location.className += "cityCountryPage"
 
         const photographersTagline = document.createElement( 'p' )
         photographersTagline.textContent = tagline
-        photographersTagline.className += "tagLine"
+        photographersTagline.className += "tagLinePage"
 
-        photographersCardLink.appendChild(img)
         photographersCardLink.appendChild(h2)
+        photographersCardLink.appendChild(location)
+        photographersCardLink.appendChild(photographersTagline)
 
         article.appendChild(photographersCardLink)
-        article.appendChild(location)
-        article.appendChild(photographersTagline)
+        article.appendChild(img)
 
         return (article)
     }
