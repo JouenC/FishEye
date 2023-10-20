800a0c74e2219346a6863ee3e840224a68fd82b0
Charles <charles.jouenne@gmail.com>
2023-10-20 10:20:27 +0200

    Appel des medias

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -6,12 +6,17 @@ async function getPhotographer() {
         const id = getUrlQuery("id")
         
         // Recherche dans les data le photographe correspnodant à l'Id
-        const photogarpherPage = data.photographers.find((photographer) => photographer.id === Number(id))
-        return photogarpherPage
+        const photogarpherData = data.photographers.find((photographer) => photographer.id === Number(id))
+
+        const {media} = data
+        const photographerMedia = media.filter(item => item.photographerId === parseInt(id));
+        return {photogarpherData, photographerMedia}
     } catch (error) {
         console.error(error)
     }
 }
+
+
 
 // Récupère les éléments de l'URL les transformant en un couple clé/valeur
 const getUrlQuery = (param) => {
@@ -25,19 +30,35 @@ const getUrlQuery = (param) => {
     }
 }
 
-async function displayDataPage(photographer) {
-    const photographersSection = document.querySelector(".photograph-header")
-    const photographerModel = photographerTemplatePage(photographer);
-    const userCardDOM = photographerModel.getUserCardDOMPage();
-    photographersSection.appendChild(userCardDOM);
-    
+async function displayMediaPage(photographerMedia) {
+    const mediaSection = document.querySelector(".photograph-media")
+    photographerMedia.forEach(media => {
+        const {getMediaCardDOM} = mediaFactory(media)
+        const article = getMediaCardDOM()
+    mediaSection.appendChild(article);
+    })
+      
+}
+
+
+async function getPhotographerMedia() {
+    try {
+        const id = getUrlQuery("id")
+        
+        const response = await fetch("data/photographers.json")
+        const data = await response.json()
+        
+        return res
+    } catch (error) {
+        console.error(error)
+    }
 }
 
 async function initPage() {
     // Récupère les datas des photographes
-    const photogarpherPage = await getPhotographer()
-    console.log(">>>", photogarpherPage)
-    displayDataPage(photogarpherPage)
+    const {photogarpherData, photographerMedia} = await getPhotographer()
+    photographerTemplatePage(photogarpherData)
+    displayMediaPage(photographerMedia)
 }
 
 initPage()
